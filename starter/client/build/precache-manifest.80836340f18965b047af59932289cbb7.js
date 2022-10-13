3d019a24d493aa0edee82066246122a9e4cd092b
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-10-13 15:25:46 -0500

    deployed amazon dynamodb to aws

---
+++ starter/client/build/precache-manifest.80836340f18965b047af59932289cbb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a63d513cc99b2eaa05b6ac570f293d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41f562fb8918bdb8f848",
+    "url": "/static/css/main.c5ae7a51.chunk.css"
+  },
+  {
+    "revision": "072bb7247cee19c5fd50",
+    "url": "/static/js/2.6b3a75fc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6b3a75fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f562fb8918bdb8f848",
+    "url": "/static/js/main.092afcf4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.092afcf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91dc04ad90fad7521de1",
+    "url": "/static/js/runtime-main.4fda0dca.js"
+  }
+]);
