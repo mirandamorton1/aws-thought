de12058ea86fe84c12153c9711e826e2f0fb2292
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-10-17 14:22:18 -0500

    created image upload route to S3 bucket

---
+++ starter/server/utils/params-config.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+  
+    const imageParams = {
+      // Replace the <My_Bucket_Name> with the name of your own S3 bucket
+      Bucket: 'user-images-b31a3bb5-4b3a-49c7-a458-9fc5b91b4fe5',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer,
+    };
+  
+    return imageParams;
+  };
+
+  module.exports = params;
